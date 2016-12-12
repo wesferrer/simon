9db926663b86e48a3e8ca2f52e0b6f9f248711f7
Wes Ferrer <wesferrer@gmail.copm>
2016-12-12 15:35:18 -0800

    working

--- js/main.js
+++ js/main.js
@@ -7,54 +7,68 @@ buttonB = document.getElementById('blue');
 buttonY = document.getElementById('yellow');
 playerChoice = [];
 computerChoice = [];
-random = Math.random();
+random = Math.floor((Math.random() * 4) + 1);
+random = random;
+
 
 
 //await player to press start
-
+function init(){
+  playerChoice = [];
+  computerChoice = [];
+  nextNum();
+}
 
 //initialize board state, wait 3 seconds, then add random number to sequence
 function nextNum(){
-  if (random <= 0.24) {
-    computerChoice.push(buttonR);
-  } else if (random >0.24 || random <=0.49 ){
-    computerChoice.push(buttonG);
-  } else if (random >0.5 || random <=0.74){
-    computerChoice.push(buttonB);
+  random = random
+  console.log(random);
+  if (random === 1) {
+    return computerChoice.push(buttonR);
+  } else if (random === 2){
+    return computerChoice.push(buttonG);
+  } else if (random === 3){
+    return computerChoice.push(buttonB);
+  } else if (random === 4){
+    return computerChoice.push(buttonY);
   } else {
-    computerChoice.push(buttonY);
+    alert('try again!');
   }
-  console.log('computerChoice[]')
 };
 
+//setTimeOut(nextNum, 3000);
 
 //animate sequence tied to random number to user
 
-
-//listen for user input
-
-//register click to array
 $('#red').on('click', function(){
   playerChoice.push(buttonR);
-  checkNum;
+  compare();
 })
 $('#green').on('click', function(){
   playerChoice.push(buttonG);
-  checkNum;
+  compare();
 })
 $('#blue').on('click', function(){
   playerChoice.push(buttonB);
-  checkNum;
+  compare();
 })
 $('#yellow').on('click', function(){
   playerChoice.push(buttonY);
-  checkNum;
+  compare();
 })
-//
-  //if click != sequence array {
-    function checkNum(){
-      if (playerChoice != computerChoice){
-        alert('Game Over!');
+
+
+function compare() {
+  random = Math.floor((Math.random() * 4) + 1);
+  for(var i = 0; i < playerChoice.length; i++) {
+    if(computerChoice[i] !=playerChoice[i]) {
+      console.log('Game Over!');
+      init();
+     }
+   } if(computerChoice.length == playerChoice.length) {
+     console.log('Match!');
+      playerChoice = [];
+      nextNum()
       }
     }
     //display game over
@@ -64,3 +78,17 @@ $('#yellow').on('click', function(){
       //}
   //}
 
+// function compare() {
+//   for (var i = 0; i < playerChoice.length; i++) {
+//     if (computerChoice[i] !== playerChoice[i]) {
+//       alert('Game Over!');
+//     }
+//     if (computerChoice.length === playerChoice.length) {
+//       alert('Match!');
+//       playerChoice = [];
+//       nextNum();
+//     }
+//   }
+// }
+
+init();
