61d5c3c2b3593f1b55592575a179e29e547b7695
Wes Ferrer <wesferrer@gmail.copm>
2016-12-14 11:22:29 -0800

    working color

--- js/main.js
+++ js/main.js
@@ -1,68 +1,107 @@
 
 //define variables
-var buttonR, buttonG, buttonB, buttonY, colorSwitch, playerChoice, computerChoice, random, score, highScore, startButton;
-buttonR = document.getElementById('red');
-buttonG = document.getElementById('green');
-buttonB = document.getElementById('blue');
-buttonY = document.getElementById('yellow');
+var playerChoice, computerChoice, score, highScore, startButton;
+
+var buttons = [
+  document.getElementById('red'),
+  document.getElementById('green'),
+  document.getElementById('blue'),
+  document.getElementById('yellow')
+];
+
 startButton = document.getElementById('start');
 playerChoice = [];
 computerChoice = [];
-random = Math.floor((Math.random() * 4) + 1);
-random = random;
 score = 0;
 highScore = 0;
 
-var tickResolution = 500;
-var ticksPerColor = 3;
-var ticksBetween = 1;
-var timerId;
-// elements
-var display = document.querySelector('div');
-var startBtn = document.querySelector('button');
-
 // event handlers
-startBtn.addEventListener('click', displayAnimals);
+$('.colors').on('click', handleColorClick);
 
-function score(){
-  document.getElementById('score').innerHTML = computerChoice.length;
+//await player to press start
+startButton.addEventListener('click', handleStartClick);
+
+
+
+// FUNCTIONS
+
+function handleStartClick() {
+  init();
+  compTurn();
 }
 
-function displayAnimals() {
+function handleColorClick() {
+  playerChoice.push(this);
+  compare();
+  document.getElementById('score').innerHTML = 'Score: ' + (computerChoice.length - 1);
+}
+
+function init(){
+  computerChoice = [];
+  document.getElementById('score').innerHTML = 'Score: 0';
+}
+
+//initialize board state, wait 3 seconds, then add random number to sequence
+function nextNum(){
+  var rnd = Math.floor(Math.random() * 4);
+  computerChoice.push(buttons[rnd]);
+};
+
+function score(){
+  document.getElementById('score').innerHTML = 'Score: ' + (computerChoice.length - 1);
+}
+
+function disableColors() {
+  $('.colors').attr('disabled', true);
+}
+
+function enableColors() {
+  $('.colors').removeAttr('disabled');
+}
+
+function animateColors() {
+
+console.log(computerChoice);
+
+  var tickResolution = 300;
+  var ticksPerColor = 3;
+  var ticksBetween = 1;
   var colorIdx = 0;
   var tickCount = 0;
-  timerId = setInterval(handleTick, tickResolution);
+  var timerId = setInterval(handleTick, tickResolution);
   function handleTick() {
     var showTick = colorIdx * (ticksBetween + ticksPerColor);
     var hideTick = ticksPerColor + colorIdx * (ticksBetween + ticksPerColor);
-    if (tickCount ===  showTick) display.innerHTML = computerChoice[colorIdx];
+    if (tickCount ===  showTick) flashButton(computerChoice[colorIdx]);
     if (tickCount ===  hideTick) {
-      display.innerHTML = '';
+      unflashButton(computerChoice[colorIdx]);
       colorIdx++;
-      if (colorIdx === computerChoice.length) clearInterval(timerId);
+      if (colorIdx === computerChoice.length) {
+        clearInterval(timerId);
+        enableColors();
+      }
     }
     tickCount++;
   }
 }
 
-function disablePlayButtons(inputs){
-  for(var i = 0; i < inputs.length; i++){
-    var button = inputs[i];
-    button.disabled = true;
-  }
+function flashButton(btn) {
+  $(btn).addClass('flash');
+  console.log('flashing btn: ', btn);
 }
-function enablePlayButtons(inputs){
-  for(var i = 0; i < inputs.length; i++){
-    var button = inputs[i];
-    button.disabled = false;
-  }
+
+function unflashButton(btn) {
+  $(btn).removeClass('flash')
+  console.log('unflash btn: ', btn);
 }
+
 function compTurn(){
-  console.log('Match!');
+  disableColors();
   playerChoice = [];
-  nextNum()
-  document.getElementById('score').innerHTML = 'Score: ' + (computerChoice.length - 1);
+  nextNum();
+  animateColors();
 }
+
 function gameOver(){
   console.log('Game Over!');
   disablePlayButtons(document.getElementsByClassName('colors'));
@@ -71,57 +110,11 @@ function gameOver(){
   }, 1000);*/
 }
 
-disablePlayButtons(document.getElementsByClassName('colors'));
 
 
-//await player to press start
-
-startButton.addEventListener('click', function(){
-  return init();
-})
-
-function init(){
-  playerChoice = [];
-  computerChoice = [];
-  nextNum();
-  enablePlayButtons(document.getElementsByClassName('colors'));
-  document.getElementById('score').innerHTML = 'Score: ';
-}
-
-//initialize board state, wait 3 seconds, then add random number to sequence
-function nextNum(){
-  console.log(random);
-  if (random === 1) {
-    return computerChoice.push(buttonR);
-  } else if (random === 2){
-    return computerChoice.push(buttonG);
-  } else if (random === 3){
-    return computerChoice.push(buttonB);
-  } else if (random === 4){
-    return computerChoice.push(buttonY);
-  } else {
-    alert('try again!');
-  }
-};
 
 //animate sequence tied to random number to user
 
-$('#red').on('click', function(){
-  playerChoice.push(buttonR);
-  compare();
-})
-$('#green').on('click', function(){
-  playerChoice.push(buttonG);
-  compare();
-})
-$('#blue').on('click', function(){
-  playerChoice.push(buttonB);
-  compare();
-})
-$('#yellow').on('click', function(){
-  playerChoice.push(buttonY);
-  compare();
-})
 
 
 function compare() {
@@ -129,7 +122,6 @@ function compare() {
   for(var i = 0; i < playerChoice.length; i++) {
     if(computerChoice[i] !== playerChoice[i]) {
       console.log('Game Over!');
-      disablePlayButtons(document.getElementsByClassName('colors'));
       setTimeout(function(){
         alert("Press Start to try again!");
       }, 1000);
@@ -139,3 +131,5 @@ function compare() {
       compTurn ();
     };
 }
+
+disableColors();
